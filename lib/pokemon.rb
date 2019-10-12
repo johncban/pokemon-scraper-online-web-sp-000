6723465ae8f1796a46c0383cc826313ff16c04ba
Juan Carlo Banayo <johncban@gmail.com>
2019-10-12 18:11:26 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,9 +2,12 @@ class Pokemon
   attr_acessor :id, :poke_name, :poke_type, :poke_hp, :db
 
   def self.save(poke_name, poke_type, db)
+    db.execute("INSERT INTO pokemon (poke_name, poke_type) VALUES (?, ?)", poke_name, poke_type)
   end
 
   def self.find(id_nu, db)
+    poke_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: poke_info[0], poke_name: poke_info[1], poke_type: poke_info[2], poke_hp: poke_info[3], db: db)
   end
 
   def initialize(id:, poke_name:, poke_type:, poke_hp: nil, db:)
