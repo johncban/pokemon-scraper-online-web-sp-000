fc4ff69d8473dbd4a4a2f2d90a67c25f810abb07
Juan Carlo Banayo <johncban@gmail.com>
2019-10-12 17:52:20 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+  attr_acessor :id, :poke_name, :poke_type, :poke_hp, :db
+
+  def self.save(poke_name, poke_type, db)
+  end
+
+  def self.find(id_nu, db)
+  end
+
+  def initialize(id:, poke_name:, poke_type:, poke_hp: nil, db:)
+    @id, @poke_name, @poke_type, @poke_hp, @db = id, poke_name, poke_type, poke_hp, db
+  end
+
+  def alater_poke_hp(new_hp, db)
+    
+  end
 end
